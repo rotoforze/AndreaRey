3c897b97a6c7ab9ab35e6a3f395f52c749ce2f1f
Alex <118481249+rotoforze@users.noreply.github.com>
2023-06-05 17:36:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+window.onload = inicio();
+
+function inicio () {
+    document.getElementById("galeria").addEventListener("click", sw); // El clicker para cambiar entre sobre mi y la galería
+}
+
+let am = document.getElementById("aboutme"); // Acortamos código
+let gal = document.getElementById("gal"); // "
+
+function sw(){
+    if(document.getElementById("gal").hidden==true){ // Cambiar a galería
+        document.getElementById("gal").hidden=false; 
+        document.getElementById("aboutme").hidden=true;
+        document.querySelector("img").src = "./IMAGE/IMG_0444.PNG"
+    }else if(document.getElementById("aboutme").hidden==true){ // Cambiar a sobre mi
+        document.getElementById("gal").hidden=true;
+        document.getElementById("aboutme").hidden=false;
+        document.querySelector("img").src = "./IMAGE/IMG_0445.PNG"
+    }
+}
